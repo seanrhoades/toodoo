0e04e9567475356b606eecae99d1c152d3de510b
Sean Rhoades <sean@seanrhoades.com>
2016-02-29 00:38:13 -0500

    Toodoo first and final commit

---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,18 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+$(document).on("ready page:load", ready);
+
+function ready() {
+  $('#new_task').on('ajax:success', newTask);
+
+ function newTask() {
+   window.location.reload();
+ }
+
+}
+
+function newTask(event, data) {
+  $('#tasks').append(data);
+  $('#task_name').val('');
+}
